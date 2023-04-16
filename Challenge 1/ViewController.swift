a15d5233c005a9046a44ae8c8e708e1282958f03
Brandon Hobbs <brandon.hobbs1998@gmail.com>
2023-04-16 00:47:19 -0500

    Challenge 1 Complete

--- Challenge 1/ViewController.swift
+++ Challenge 1/ViewController.swift
@@ -8,12 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var messageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        messageLabel.text = "Go Ahead... Press A Button"
+
     }
 
 
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        messageLabel.text = "You Are Awesome!"
+    }
+    @IBAction func anotherButtonPressed(_ sender: UIButton) {
+        messageLabel.text = "You Are Great!"
+    }
 }
 
